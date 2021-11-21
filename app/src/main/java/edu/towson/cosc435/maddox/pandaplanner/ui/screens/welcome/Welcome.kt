2f364a4c4a2247d3e34b55501c3f857be38cdd0e
Andrew M <83321889+ChefYaboiardee@users.noreply.github.com>
2021-11-21 12:25:57 -0500

    started getting the welcome screen in order

--- app/src/main/java/edu/towson/cosc435/maddox/pandaplanner/ui/screens/welcome/Welcome.kt
+++ app/src/main/java/edu/towson/cosc435/maddox/pandaplanner/ui/screens/welcome/Welcome.kt
@@ -1,2 +1,43 @@
 package edu.towson.cosc435.maddox.pandaplanner.ui.screens.welcome
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.lifecycle.ViewModel
+
+@Composable
+fun Welcome(){
+    val vm = WelcomeViewModel()
+    val randomMessage1 = vm.randomMessage1
+    val randomMessage2 = vm.randomMessage2
+
+    Column(modifier = Modifier.clickable { //TODO implement navigation
+         },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        ) {
+        //Welcome message
+        Text(text = "Welcome to Panda Planner!")
+
+        Row() {
+            //Column for random message 1
+            Column() {
+                Text(text = randomMessage1)
+            }
+            //Column for random message 2
+            Column() {
+                Text(text = randomMessage2)
+            }
+        }
+    }
+}
+//TODO apply our color theme
+//TODO implement random dad joke api
+
+
--- app/src/main/java/edu/towson/cosc435/maddox/pandaplanner/ui/screens/welcome/WelcomeViewModel.kt
+++ app/src/main/java/edu/towson/cosc435/maddox/pandaplanner/ui/screens/welcome/WelcomeViewModel.kt
@@ -1,4 +1,39 @@
 package edu.towson.cosc435.maddox.pandaplanner.ui.screens.welcome
 
-class WelcomeViewModel {
+import android.content.Context
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WelcomeViewModel : ViewModel() {
+    private val _randomMessage1 by mutableStateOf("")
+    val randomMessage1 = _randomMessage1
+
+    private val _randomMessage2 by mutableStateOf("")
+    val randomMessage2 = _randomMessage2
+
+    init {
+        viewModelScope.launch(context=Dispatchers.IO) { loadMessages() }
+    }
+
+    private suspend fun loadMessages(){
+        viewModelScope.launch {
+        //some function to get messages
+        }
+    }
 }
+//code for random dad joke api
+//val client = OkHttpClient()
+//
+//val request = Request.Builder()
+//    .url("https://dad-jokes.p.rapidapi.com/joke/search?term=cow")
+//    .get()
+//    .addHeader("x-rapidapi-host", "dad-jokes.p.rapidapi.com")
+//    .addHeader("x-rapidapi-key", "SIGN-UP-FOR-KEY")
+//    .build()
+//
+//val response = client.newCall(request).execute()
