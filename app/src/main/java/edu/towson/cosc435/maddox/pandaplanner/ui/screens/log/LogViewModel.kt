d343ed6c8ce98c030b09cc70f00b2746c188f1d0
JaVonne Harrison <javonneharrison@JaVonnes-MBP.fios-router.home>
2021-12-11 21:46:44 -0500

    added log and logViewModel + some changes to EventRow

--- app/src/main/java/edu/towson/cosc435/maddox/pandaplanner/ui/screens/log/LogViewModel.kt
+++ app/src/main/java/edu/towson/cosc435/maddox/pandaplanner/ui/screens/log/LogViewModel.kt
@@ -1,4 +1,24 @@
 package edu.towson.cosc435.maddox.pandaplanner.ui.screens.log
 
-class LogViewModel {
+import android.app.Application
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import edu.towson.cosc435.maddox.pandaplanner.data.EventsRepository
+import edu.towson.cosc435.maddox.pandaplanner.data.IEventsRepository
+import edu.towson.cosc435.maddox.pandaplanner.model.Event
+
+
+class LogViewModel(app: Application): ViewModel(){
+
+    private val _listLog : MutableState<List<Event>> = mutableStateOf(listOf())
+    val listLog: State<List<Event>> = _listLog
+
+    private val _repositoryEvent: IEventsRepository = EventsRepository(app)
+
+    init{
+        _listLog.value = _repositoryEvent.getEvents()
+    }
 }
